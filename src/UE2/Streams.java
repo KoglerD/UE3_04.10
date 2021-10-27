67558e19db98a55834bdfbd97ca5db8bf27025b2
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2021-10-27 23:44:05 +0200

    UE2 fast fertig: Test: testSumUpHashCodes funkt noch nicht 47 Tests gehen

--- src/UE2/Streams.java
+++ src/UE2/Streams.java
@@ -1,5 +1,6 @@
 package UE2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -44,51 +45,87 @@ public class Streams {
         return list;
     }
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
-        Comparator<Weapon> comparatorDamage = (w1, w2) -> {
+        /*Comparator<Weapon> comparatorDamage = (w1, w2) -> {
             return w1.getDamage() - w2.getDamage();
         };
-        weapons.sort(comparatorDamage);
+        //weapons.sort(comparatorDamage);*/
+        if (weapons.isEmpty()){
+            return null;
+        }
+        weapons.sort(Comparator.comparing(Weapon::getDamage));
         return weapons.get(0);
     }
 
     public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
-        Comparator<Weapon> comparatorDamage = (w1, w2) -> {
+
+        /*Comparator<Weapon> comparatorDamage = (w1, w2) -> {
             return w2.getMinStrength() - w1.getMinStrength();
         };
         weapons.sort(comparatorDamage);
-        return weapons.get(0);
+        return weapons.get(0);*/
+        //Weapon weapon = weapons.sort(Comparator.comparing());
+        try {
+            if (weapons.isEmpty()){
+                return null;
+            }
+                weapons.sort(Comparator.comparing(Weapon::getMinStrength));
+                return weapons.get(weapons.size()-1);
+        }catch (IndexOutOfBoundsException ioobe){
+            System.out.println("Index zu hoch");
+            return weapons.get(weapons.size());
+        }
+
     }
 
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
-       // weapons = (List<Weapon>)weapons.stream().reduce(0,(x, y) -> x.getDamageType().equals(DamageType.MISSILE));
-        return weapons;
+        //List<Weapon> weList = new ArrayList<>();
+        /*for (int i = 0; i < weapons.size(); i++) {
+            if (weapons.get(i).getDamageType() == (DamageType.MISSILE)) {
+                weapons.remove(i);
+                //weList.add(weapons.get(i));
+            }
+        }*/
+        List<Weapon> weList = weapons.stream().filter(weapon -> weapon.getDamageType()==DamageType.MISSILE).collect(Collectors.toList());
+        //weapons = (List<Weapon>)weapons.stream().reduce(0,(x, y) -> x.getDamageType()==equals(DamageType.MISSILE));
+        return weList;
     }
 
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
-        return new Weapon();
+        if (weapons.isEmpty()){
+            return null;
+        }
+        weapons.sort(Comparator.comparing(weapon -> weapon.getName().length()));
+        return weapons.get(weapons.size()-1);
     }
 
     public List<String> toNameList(List<Weapon> weapons) {
-        return null;
+        List<String> nameList = new ArrayList<>();
+        weapons.stream().forEach(weapon -> nameList.add(weapon.getName()));
+        return nameList;
     }
 
     public int[] toSpeedArray(List<Weapon> weapons) {
-        return null;
+        List<Integer> speedList = new ArrayList<>();
+        weapons.stream().forEach(weapon -> speedList.add(weapon.getSpeed()));
+        return speedList.stream().mapToInt(i -> i).toArray();
     }
 
     public int sumUpValues(List<Weapon> weapons) {
-        return 0;
+        //weapons.stream().forEach(weapon -> list.add(weapon.getValue()));
+        return weapons.stream().mapToInt(weapon -> weapon.getValue()).sum();
+
     }
 
     public long sumUpHashCodes(List<Weapon> weapons) {
-        return 0;
+        return weapons.stream().mapToLong(weapon -> weapon.hashCode()).sum();
     }
 
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
+        weapons.stream().distinct();
         return weapons;
     }
 
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
-        //implement this
+        weapons.stream().forEach(weapon -> weapon.setValue((int) weapon.getValue()*110/100));
     }
 }
